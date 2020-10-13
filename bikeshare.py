151c6958039ee714b6eb1e6fda42dd465ced2529
sara-atishev <72482043+sara-atishev@users.noreply.github.com>
2020-10-13 15:58:53 -0700

    Docs: add entry options to returns in docstring

--- bikeshare.py
+++ bikeshare.py
@@ -16,9 +16,9 @@ def get_filters():
     Asks user to specify a city, month, and day by which to filter the bikeshare data.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - name of the city to analyze (chicago, new york city, washington, or all)
+        (str) month - name of the month to filter by (january, february, ..., june), or "all" to apply no month filter
+        (str) day - name of the day of week to filter by (monday, tuesday, ..., sunday), or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!\n')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
